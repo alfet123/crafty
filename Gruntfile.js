7ffde9e04c36caa85a41d40d096f0a612ccef8ac
Алексей Фетисов <alfet123@gmail.com>
2016-04-16 13:32:47 +0500

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+"use strict";
+
+module.exports = function(grunt) {
+  require("load-grunt-tasks")(grunt);
+
+  var config = {
+    pkg: grunt.file.readJSON("package.json"),
+
+    less: {
+      style: {
+        files: {
+          "css/style.css": "less/style.less"
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        processors: [
+          require("autoprefixer")({browsers: "last 2 versions"})
+        ]
+      },
+      style: {
+        src: "css/*.css"
+      }
+    },
+
+    watch: {
+      style: {
+        files: ["less/**/*.less"],
+        tasks: ["less", "postcss"],
+        options: {
+          spawn: false,
+          livereload: true
+        }
+      }
+    }
+  };
+  
+
+
+  // Не редактируйте эту строку
+  config = require("./.gosha")(grunt, config);
+
+  grunt.initConfig(config);
+};
